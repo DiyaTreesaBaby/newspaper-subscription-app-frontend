da3c098180970fac867a5cb1bf225d1b9c8753fc
iyaTreesaBaby <diyababy1010@gmail.com>
2024-07-25 22:28:01 +0530

    AddSearch created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import AddSubscribers from './components/AddSubscribers';
+import AddSearch from './components/AddSearch';
 
 function App() {
   return (
-    <AddSubscribers/>
+    <AddSearch/>
   );
 }
 
---
+++ src/components/AddDelete.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AddDelete = () => {
+  return (
+    <div>
+
+
+
+
+        
+    </div>
+  )
+}
+
+export default AddDelete
