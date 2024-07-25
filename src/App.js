59645d1bd773e6f2f72a9fd6ee608d359a7596c2
iyaTreesaBaby <diyababy1010@gmail.com>
2024-07-26 00:48:07 +0530

    HomePage Added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import './App.css';
 import AddSubscribers from './components/AddSubscribers';
 import AddSearch from './components/AddSearch';
 import AddView from './components/AddView';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddDelete from './components/AddDelete';
 
 function App() {
   return (
-    <AddView/>
+   <BrowserRouter>
+   <Routes>
+   <Route path='/' element={<Home/>} />
+    <Route path='/View' element={<AddView/>} />
+    <Route path='/Search' element={<AddSearch/>} />
+    <Route path='/Delete' element={<AddDelete/>} />
+    <Route path='/Add' element={<AddSubscribers/>} />
+
+
+   </Routes>
+   
+   
+   
+   </BrowserRouter>
   );
 }
 
