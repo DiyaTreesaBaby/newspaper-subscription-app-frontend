7f086b59c3a413a294a60ead3d15ed0828077ab3
iyaTreesaBaby <diyababy1010@gmail.com>
2024-07-26 00:31:44 +0530

    AddList created

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import AddSubscribers from './components/AddSubscribers';
 import AddSearch from './components/AddSearch';
+import AddView from './components/AddView';
 
 function App() {
   return (
-    <AddSearch/>
+    <AddView/>
   );
 }
 
---
+++ src/components/AddView.jsx
@@ -0,0 +1,150 @@
+import React from 'react'
+
+const AddView = () => {
+  return (
+    <div>
+<div className="cntainer">
+    <div className="rw">
+        <div className="cl cl-12 cl-sm-12 cl-md-12 cl-lg-12 cl-xl-12 cl-xxl-12">
+            <div className="rw g-3">
+                <div className="cl cl-12 cl-sm-12 cl-md-12 cl-lg-12 cl-xl-12 cl-xxl-12">
+                
+                <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">NO.</th>
+      <th scope="col">NAME</th>
+      <th scope="col">PHONE NUMBER</th>
+      <th scope="col">ADDRESS</th>
+      <th scope="col">PINCODE</th>
+      <th scope="col">DISTRICT</th>
+      <th scope="col">VILLAGE OFFICE</th>
+      <th scope="col">EMAIL ID</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>9567834411</td>
+      <td>45 Abbey Road,London,UK </td>
+      <td>NW8 9AY</td>
+      <td>Westminster(London,Uk)</td>
+      <td>WestMinster</td>
+      <td>mark01@gmail.com</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>9811122233</td>
+      <td>12 Sarojini Nagar,New Delhi</td>
+      <td>110023</td>
+      <td>New Delhi</td>
+      <td>New Delhi</td>
+      <td>jacob@gmail.com</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Ananya Iyer</td>
+      <td>9922334455</td>
+      <td>34 Nehru street,Ahmadabad,Gujarat</td>
+      <td>380001</td>
+      <td>Ahmedabad</td>
+      <td>Ahmedabad</td>
+      <td>ananya34@gamil.com</td>
+     
+    </tr>
+    <tr>
+      <th scope="row">4</th>
+      <td>Kavitha Reddy</td>
+      <td>9988776655</td>
+      <td>Jayanagar,Bengaluru,Karnataka</td>
+      <td>560041</td>
+      <td>Bengaluru</td>
+      <td>Bengaluru</td>
+      
+      <td>kavitha@gmail.com</td>
+     
+    </tr>
+    <tr>
+      <th scope="row">5</th>
+      <td>Priya Sharma</td>
+      <td>9820123456</td>
+      <td>123 Park Street,Kolkata,West Bengal</td>
+      <td>700016</td>
+      <td>Kolkata</td>
+      <td>Kolkata</td>
+      <td>priya@gmail.com</td>
+     
+    </tr>
+    <tr>
+      <th scope="row">6</th>
+      <td>Ramesh Singh</td>
+      <td>9875654321</td>
+      <td>456 Marine Drive,Mumbai,Maharashtra</td>
+      <td>40020</td>
+      <td>Mumbai</td>
+      <td>Mumbai</td>
+      <td>rameshsingh@gmail.com</td>
+     
+    </tr>
+    <tr>
+      <th scope="row">7</th>
+      <td>Sneha Patil</td>
+      <td>912345678</td>
+      <td>789 MG Road,Pune,Maharashtra</td>
+      <td>411001</td>
+      <td>Pune</td>
+      <td>Pune</td>
+      <td>sneha897@gmail.com</td>
+     
+    </tr>
+    <tr>
+      <th scope="row">8</th>
+      <td>Ahmed Ali</td>
+      <td>971501234567</td>
+      <td>789 Palm Jumeirah,Dubai,UAE</td>
+      <td>00000</td>
+      <td>Dubai</td>
+      <td>Dubai</td>
+      <td>aliahmed@gmail.com</td>
+     
+    </tr>
+    <tr>
+      <th scope="row">9</th>
+      <td>Neha Bhatia</td>
+      <td>9887766554</td>
+      <td>404 MG Road,Jaipur,rajasthan</td>
+      <td>302001</td>
+      <td>Jaipur</td>
+      <td>Jaipur</td>
+      <td>neha456@gmail.com</td>
+     
+    </tr>
+    <tr>
+      <th scope="row">10</th>
+      <td>Sunita Sharma</td>
+      <td>9872234856</td>
+      <td>909 Connaught Place,New Delhi</td>
+      <td>110001</td>
+      <td>New Delhi</td>
+      <td>New Delhi</td>
+      <td>sunita@gmail.com</td>
+     
+    </tr>
+  </tbody>
+</table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+    </div>
+  )
+}
+
+export default AddView
