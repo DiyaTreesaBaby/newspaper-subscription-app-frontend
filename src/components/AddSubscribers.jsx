0546da5a325217d051973c2d8dfb827416ae0bbd
iyaTreesaBaby <diyababy1010@gmail.com>
2024-07-25 22:12:40 +0530

    AddSubscribers created

---
+++ src/components/AddSubscribers.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+const AddSubscribers = () => {
+  return (
+    <div>
+<div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+<div className="row g-3">
+    <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+    <label htmlFor="" className="form-label">SUBSCRIBER NAME</label>
+    <input type="text" className="form-control" />
+
+    </div>
+    <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+    <label htmlFor="" className="form-label">PHONE NUMBER</label>
+    <input type="text" className="form-control" />
+    </div>
+    <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+    <label htmlFor="" className="form-label">ADDRESS</label>
+    <input type="text" className="form-control" />
+    </div>
+    <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+    <label htmlFor="" className="form-label">PINCODE</label>
+    <input type="text" className="form-control" />
+    </div>
+    <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+    <label htmlFor="" className="form-label">DISTRICT</label>
+    <input type="text" className="form-control" />
+    </div>
+    <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+    <label htmlFor="" className="form-label">VILLAGE OFFICE</label>
+    <input type="text" className="form-control" />
+    </div>
+    <div className="col col-12 col-sm-12 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+    <label htmlFor="" className="form-label">EMAIL ID</label>
+    <input type="text" className="form-control" />
+    </div>
+    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+    <button className="btn btn-danger">SUBMIT</button>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default AddSubscribers
+
